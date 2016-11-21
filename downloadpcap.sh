8a491c118d27f9c3a73c218849e47a3d931afd14
tclasen <ToryClasen@Meertecha.com>
2016-11-21 14:03:21 -0500

    Create downloadpcap.sh

---
+++ downloadpcap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# downloadpcap.sh
+　
+for i in illauth.pcap theft.pcap email.pcap notube.pcap dairystock.pcap; do
+  echo "Downloading pcap $i"
+  wget -q https://www.bro.org/static/workshop-11/traces/$i
+done
